c976e0ed7f36680bada658929a642ac4b7379586
Mukmilatul <muqmilat@gmail.com>
2025-11-16 08:04:01 -0800

    javascript added

---
+++ script.js
@@ -0,0 +1,8 @@
+let profileHead = document.querySelector('.profile1'); 
+let hideProfile = document.querySelector('.hide')
+
+profileHead.addEventListener('click', ()=>{
+    hideProfile.style.display="block"; 
+    hideProfile.style.cursor='pointer';
+
+})
